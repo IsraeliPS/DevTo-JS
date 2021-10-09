ad6fbfe769ba193ef62e5d3ad3d348145578ed76
root <root@COROLA.localdomain>
2021-10-08 19:16:00 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mySidebar").style.overflow="show";
+    // document.getElementById("main-sidebar").style.marginLeft = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = 0
+    // document.getElementById("mySidebar").style.overflow="hidden";
+    // document.getElementById("mySidebar").style.marginLeft= 0;
+    // document.getElementById("main-sidebar").style.marginLeft= 0;
+}
