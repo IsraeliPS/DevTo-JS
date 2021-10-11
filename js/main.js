7ffa4a3272bd1eef56b9c731434bb58831f4b613
Fany Alvarez <alvarezpestefania@outlook.com>
2021-10-10 19:33:27 -0500

    add_brach_filter

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ function closeNav() {
     // document.getElementById("mySidebar").style.marginLeft= 0;
     // document.getElementById("main-sidebar").style.marginLeft= 0;
 }
+
+const getPosts = () => {
+    let response;
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            response  = JSON.parse( xhttp.responseText );
+            //console.log( response )
+        }
+    };
+    xhttp.open("GET", "https://proyecto-devto-default-rtdb.firebaseio.com//Posts/posts.json", false);
+    xhttp.send();
+    return response 
+}
+let allpost = getPosts()
+
+console.log( allpost )
