7215a584743e70e3cac541bc04f77788b4acb4f6
Israelí Pérez <israeli.ps@gamil.com>
2021-10-10 22:46:23 -0500

    add posts

---
+++ js/index_post.js
@@ -0,0 +1,5 @@
+var data = localStorage.getItem("post");
+localStorage.clear(); //clean the localstorage
+var value = JSON.parse(data)
+console.log(data)
+console.log(value)
--- js/main.js
+++ js/main.js
@@ -29,57 +29,6 @@ function getData(){
     return postObject
 }
 
-function createPost(postObject){
-    let resp
-
-    $.ajax({
-        method:"POST",
-        url:"https://proyecto-devto-default-rtdb.firebaseio.com/Posts/posts.json",
-        data:JSON.stringify(postObject),
-        success:(response)=>{
-            resp=response
-            console.log(response)
-        },
-        error:(error)=>{
-            console.log(error)
-        },
-        async:false
-    })
-    return resp
-}
-
-function updatePost(idPost,objectPost){
-    $.ajax({
-        method:"PATCH",
-        url:`https://proyecto-devto-default-rtdb.firebaseio.com/Posts/posts/${idPost}.json`,
-        data:JSON.stringify(objectPost),
-        success:(response)=>{
-            console.log(response)
-        },
-        error:(error)=>{
-            console.log(error)
-        },
-        async:false
-    })
-}
-
-function deletePost(idPost){
-    $.ajax({
-        method:"DELETE",
-        url:`https://proyecto-devto-default-rtdb.firebaseio.com/Posts/posts/${idPost}.json`,
-        data:idPost,
-        success:(response)=>{
-            // console.log("El registro fue eliminado correctamente")
-            console.log(response)
-            
-        },
-        error:(error)=>{
-            console.log(error)
-        },
-        async:false
-    })
-}
-
 function convertArray(postObject){
     let arrayPost=[]
     for (const key in postObject) {
@@ -197,8 +146,7 @@ function printPost(){
                         divReactions.appendChild(pHeart)
 
                             let spanChat=document.createElement("span")
-                            // spanChat.classList.add("col-3")
-
+                            
                                 let chat=document.createElementNS(SVG_NS,"svg")
                                 chat.setAttributeNS(null,"width","16")
                                 chat.setAttributeNS(null,"height","16")
@@ -238,109 +186,4 @@ function printPost(){
     })
 }
 
-//apoyo para crear nuevos posts
-let newObject={
-    coments:"18",
-    cover_image:"https://images.party-city.es/images/link/19_BALL1154ar_097_L2.jpg",
-    data_created:"02-11-2021",
-    hashtags:[
-        "javascript",
-        "css"
-    ],
-    likes:5,
-    post_text:"algo mas deberia ir aqui",
-    title:"El uso del html en el bootcamp",
-    usuario:{
-        img:"https://3.bp.blogspot.com/-JfL1o7oSnKI/VmodObHF9cI/AAAAAAAABLY/nKKRXw0-yiU/s1600/homero_456_336.jpg",
-        name:"Bart",
-        user_id:"Bart.Github"
-    }
-}
-getData()
-
 printPost()
-
-
-
-/*
-
-function validar(){
-    let objectData={name:"",phone:"",github:"",urlGithub:"",}
-    $(".form-group .register").each(function(index){
-        if (this.value){
-            objectData[this.name]=this.value
-            this.value=""
-        }
-        else{
-            alert(`Todos los campos son requeridos`)
-            objectData={}
-        }
-    })
-    return objectData
-}
-
-
-function launchModal(data){
-    let {name,phone,github,urlGithub,id}=data
-    let inputsComplete=$(".form-group .modalPerson")
-    
-    inputsComplete.each(function(index){
-        switch(this.name){
-        case "id":
-            this.value=id    
-            break;
-        case "name":
-            this.value=name
-            break;
-        case "phone":
-            this.value=phone
-            break;
-        case "github":
-            this.value=github
-            break;
-        case "urlGithub":
-            this.value=urlGithub
-            break;
-        }
-    })
-    
-}
-
-
-function preUpdate(){
-    let objectPerson={name:"",phone:"",github:"",urlGithub:""}
-    let inputs=$(".form-group .modalPerson")
-    let id=""
-    inputs.each(function(index){
-        switch(this.name){
-        case "id":
-            id=this.value
-            break;
-        case "name":
-            objectPerson.name=this.value
-            break;
-        case "phone":
-            objectPerson.phone=this.value
-            break;
-        case "github":
-            objectPerson.github=this.value
-            break;
-        case "urlGithub":
-            objectPerson.urlGithub=this.value
-            break;
-        }
-    })
-    updatePerson(id,objectPerson)
-    printCard()
-    $("#exampleModal").modal("hide")
-}
-
-$("#btnAgregar").click(()=>{
-    let data=validar()
-    data?createPerson(data):console.log("datos erroneos")
-})
-
-$("#updateData").click(()=>preUpdate())
-
-printCard()
-*/
